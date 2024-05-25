63d21e5c72bee2f7d4b6c94627b58881e4f04791
Chris Wininger <chris.wininger@gmail.com>
2024-05-25 09:30:33 -0400

    adding a backup script for future reference

---
+++ backup-laptop.sh
@@ -0,0 +1,5 @@
+# this first first line should really end with backup-2024 but we'll need to fix the destination first :-)
+sudo rsync -avxP /media/chris/bigdata /media/chris/backup-2024/bigdata
+
+sudo rsync -avxP /home /media/chris/backup-2024
+
